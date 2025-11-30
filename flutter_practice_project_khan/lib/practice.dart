1d3689aee8da68b9c5c0dca76e45b82ac082ba0f
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-11-30 15:52:13 +0600

    Add mu Practice Session

---
+++ flutter_practice_project_khan/lib/practice.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+void main(){
+  print("hello Md Rahim Khan object");
+
+  stdout.write("Enter your name: ");
+
+  // var name = stdin.readLineSync();
+  //
+  // print("wellcome ,$name");
+  // stdout.write("Wellcome , $name");
+
+  var farari = new Car();
+  new Car();
+  // Car();
+
+
+  int? a;
+  print(a);
+
+  BigInt b=BigInt.parse("16238463824322462342342");
+  print(b);
+
+  double d=19.8;
+  print(" ---");
+  stdout.write(d);
+  num ab = 20.0;
+  print(" ---");
+  print(ab);
+  int c = 4;
+  print(c);
+
+  
+}
+
+
+
+
+class Car{
+  Car();
+}
